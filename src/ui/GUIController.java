ea9ee53db401d1c01190c870a4766c0cf5ab3b12
Santiago <santiagocardenasmontes@gmail.com>
2022-05-05 01:18:48 -0500

    V3

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -1,4 +1,40 @@
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class GUIController {
+    //Attributes
+
+    private AddGUI addModule;
+
+    public GenerateGUI generateGUI;
+
+    public SearchGUI searchGUI;
+
+    public UpdateGUI updateGUI;
+
+    public String userLoggedIn;
+
+
+    @FXML
+    private Menu toGenerateWindow;
+
+    @FXML
+    private Menu toAddWindow;
+
+    @FXML
+    private Menu toSearchWindow;
+
+    @FXML
+    private Pane mainPane;
+
+    private Stage mainStage;
+
+    public GUIController()
+    {
+
+    }
 }
